b4a869b1df22f251c2c96dc41b9401a60c438ae1
Joshua <josuealejandrof926@gmail.com>
2024-06-12 23:06:05 +0200

    Add game Over styles, and logic

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -5,11 +5,11 @@ const GameOver = ({ winner, gameState, restartGame }) => {
     <div className={`gameOver ${gameState !== 'playing' ? 'active' : ''}`}>
       <div className="card">
         <h1>Game Over</h1>
-        <p>{winner} Won the game!</p>
+        <p className={`${winner && winner.toLowerCase()}`}>{winner} Won the game!</p>
 
         <div className="buttons">
           <button onClick={restartGame}>Play Again</button>
-          <button>Quit</button>
+          {/* <button>Quit</button> */}
         </div>
       </div>
     </div>
