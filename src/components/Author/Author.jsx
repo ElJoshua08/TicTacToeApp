6f9e779e8cf2458df53dcdec969b1d98173811f6
Joshua <josuealejandrof926@gmail.com>
2024-06-15 16:58:21 +0200

    Fixed title, start btn, and link target

--- src/components/Author/Author.jsx
+++ src/components/Author/Author.jsx
@@ -4,8 +4,13 @@ const Author = ({ authorName, authorLink }) => {
   return (
     <div className="author">
       <p>
-        Made with <strong>love</strong> by {" "}
-        <a href={authorLink}>{authorName}</a>
+        Made with <strong>love</strong> by{' '}
+        <a
+          target="_blank"
+          href={authorLink}
+        >
+          {authorName}
+        </a>
       </p>
     </div>
   );
